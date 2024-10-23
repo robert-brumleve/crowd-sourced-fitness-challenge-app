d2d40f925cad665e1124f2e0605cadfcad03b312
Robert Brumleve <107527711+robert-brumleve@users.noreply.github.com>
2024-10-23 03:14:12 +0000

    added email input to account creation

--- src/account/NewAccount.js
+++ src/account/NewAccount.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const NewAccount = () => {
+const App = () => {
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [password, setPassword] = useState('');    
+    const [email, setEmail] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/register', { username, password });
+            const response = await axios.post('http://localhost:5000/register', { username, email, password });
             console.log('User created:', response.data);
         } catch (error) {
             console.error('Error creating user:', error);
@@ -26,11 +27,19 @@ const NewAccount = () => {
                     onChange={(e) => setUsername(e.target.value)}
                     required
                 />
+                
                 <input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <button type="submit">Create Account</button>
@@ -39,4 +48,4 @@ const NewAccount = () => {
     );
 };
 
-export default NewAccount;
+export default App;
