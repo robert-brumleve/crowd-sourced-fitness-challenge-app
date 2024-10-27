3466dbd2462af5b65dd9e0d123e9fe897c25ba3b
Yohan Moon <yohan.moon@outlook.com>
2024-10-27 16:45:19 -0700

    init

---
+++ src/chat/ChatRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ChatRoom() {
+  return (
+    <div className="chat-room">
+      // The chat room components will go here
+    </div>
+  );
+}
+
+export default ChatRoom;
