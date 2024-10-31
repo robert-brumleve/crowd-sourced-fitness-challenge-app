96fa23e33b9436ee7336d7295cdb152e637c8291
Yohan Moon <yohan.moon@outlook.com>
2024-10-31 12:50:28 -0700

    combined ChallengeList.js and UserInfo.js into ChatList

--- src/chat/ChatList.js
+++ src/chat/ChatList.js
@@ -1,15 +1,127 @@
-import React from 'react'
-import './chatroom.css'
-import UserInfo from './chatlist/UserInfo'
-import ChallengeList from './chatlist/ChallengeList'
+import React from "react";
+import "./chatroom.css";
+
+import { useUserStore } from "./lib/UserStore";
 
 const ChatList = () => {
+
+  const{currentUser} = useUserStore();
+
   return (
     <div class="chatlist">
-      <UserInfo/>
-      <ChallengeList/>
-    </div>
-  )
-}
 
-export default ChatList
+      {/* USER INFO */}
+
+      <div class="userinfo">
+        <div class="user">
+          <img src={currentUser.photo || "/img//chat/avatar.png"} alt="" />
+          <h5>{currentUser.displayName}</h5>
+        </div>
+
+        {/*user information - to add later*/}
+        <div class="icons"></div>
+      </div>
+
+      {/* USER'S CHALLENGE LIST' */}
+
+      <div class="challengelist">
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/chat/fitness.png" alt="" />
+          <div class="texts">
+            <span>:challenge name</span>
+            <p>latest message</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
