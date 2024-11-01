aa08f7fdcd9688176084bb73e4be1b39b6debc21
Yohan Moon <yohan.moon@outlook.com>
2024-10-31 23:24:03 -0700

    for data management

---
+++ src/chat/lib/UserStore.js
@@ -0,0 +1,26 @@
+//This code is based from
+// https://zustand.docs.pmnd.rs/getting-started/introduction
+// and https://firebase.google.com/docs/firestore/query-data/get-data
+
+import { doc, getDoc } from "firebase/firestore";
+import { create } from "zustand";
+import { db } from "./firebase";
+
+export const useUserStore = create((set) => ({
+  currentUser: null,
+  isLoading: true,
+  fetchUserInfo: async (uid) => {
+    if (!uid) return set({ currentUser: null, isloading: false });
+    try {
+      const docRef = doc(db, "users", uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        set({ currentUser: docSnap.data(), isLoading: false });
+      } else {
+        set({ currentUser: null, isLoading: false });
+      }
+    } catch (err) {
+      return set({ currentUser: null, isloading: false });
+    }
+  },
+}));
