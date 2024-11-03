2b420f3ba5d2441b995eaffe6c8b24e3fd23eb96
Yohan Moon <yohan.moon@outlook.com>
2024-11-02 20:10:29 -0700

    added chatroom for build purpose

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import UpdateChallenge from "./challenges/UpdateChallenge";
 import DashBoard from "./dashboard/DashBoard";
 import AllChallenges from "./challenges/AllChallenges";
 import NewAccount from "./account/NewAccount";
+import ChatRoom from "./chat/ChatRoom";
 
 const App = () => {
   return (
@@ -26,6 +27,8 @@ const App = () => {
         <Route path="/challenges/new" element={<NewChallenge />} />
         <Route path="/challenges/:cId" element={<UpdateChallenge />} />
         <Route path="*" element={<Navigate to="/" />} />
+        {/* chat room should possibly be /:uid/chatroom/:chat_id */}
+        <Route path="/chatroom" element={<ChatRoom/>}/>
       </Routes>
     </BrowserRouter>
   );
