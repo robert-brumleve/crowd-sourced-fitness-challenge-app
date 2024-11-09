51f448f7e392e8da7d0e660db39c6204871dd5bb
Yohan Moon <yohan.moon@outlook.com>
2024-11-08 18:17:38 -0800

    fixed to match sender name  and message

--- frontend/src/chat/ChatBox.js
+++ frontend/src/chat/ChatBox.js
@@ -15,13 +15,11 @@ import { useUserStore } from "./lib/UserStore";
 const ChatBox = () => {
   const [text, setText] = useState("");
   const [chat, setChat] = useState();
-  const [sender, setSender] = useState();
-  const [senderPic, setSenderPic] =useState();
   
-
   const { chatId, user } = useChatStore();
   const { currentUser } = useUserStore();
   const lastMessageRef = useRef(null);
+
   /* TODO fix auto scroll not working atm*/
   useEffect(() => {
     if (lastMessageRef.current) {
@@ -29,17 +27,24 @@ const ChatBox = () => {
     }
   }, []);
 
-  /*fetch messages*/
-  useEffect(() => {
-    const unsub = onSnapshot(doc(db, "chats", chatId), (res) => {
-      setChat(res.data());
+useEffect(() => {
+  const unsub = onSnapshot(doc(db, "chats", chatId), async(res) => {
+    const items = res.data().messages;
+
+    const promises = items.map(async(item)=>{
+      const senderDocRef = doc(db, "users", item.senderId);
+      const senderDocSnap = await getDoc(senderDocRef);
+      const sender = senderDocSnap.data();
+      return{...item, sender};
     });
+    const senderData = await Promise.all(promises);
+    setChat(senderData);
+  });
 
-    return () => {
-      unsub();
-    };
-  }, [chatId]);
-
+  return () => {
+    unsub();
+  };
+}, [chatId]);
   /*TODO:fetch other user info from messager senderid*/
 
   /*TODO: handle image upload*/
@@ -87,35 +92,6 @@ const ChatBox = () => {
     setText("");
   };
 
-  /* Retrieve message sender's name to diplay on each msg*/
-useEffect(()=>{
-  
-  const unsub = onSnapshot(doc(db, "chats", chatId), async (res) => {
-      
-    const items = res.data().messages;
-    
-    const promises = items.map(async(item)=>{
-      const senderDocRef = doc(db, "users", item.senderId);
-      const senderDocSnap = await getDoc(senderDocRef);
-      const sender = senderDocSnap.data();
-
-      return{...item, sender};
-    });
-    const senderData = await Promise.all(promises);
-    
-    //set sender display for each message
-    senderData.map(async(data)=>{
-      
-      setSender(data.sender.displayName);
-      setSenderPic(data.sender.photoURL);
-      //console.log(senderPic);
-    })
-  });
-
-  return()=>{
-    unsub();
-  }
-},[chatId]);
   
 
   return (
@@ -132,7 +108,7 @@ useEffect(()=>{
 
       {/* Message Section */}
       <div className="center">
-        {chat?.messages?.map((message) => (
+        {chat?.map((message) => (
           <div
             className={
               message.senderId === currentUser.uid ? "message mine" : "message"
@@ -141,13 +117,12 @@ useEffect(()=>{
           >
             {/* show image and name if message is not current user's */}
             {message.senderId !== currentUser.uid && 
-            <img src={ senderPic || "/img/chat/avatar.png" } alt="." />}
+            <img src={ message.sender.photoURL || "/img/chat/avatar.png" } alt="." />}
 
             <div className="texts">
               {message.senderId !== currentUser.uid && (
-                <span>{sender}</span>
+                <span>{message.sender.displayName}</span>
               )}
-
               {message.img && <img src={message.img} alt="" />}
               <p>{message.text}</p>
               <span>{message.createdAt.toDate().toLocaleDateString() + " @ "+
