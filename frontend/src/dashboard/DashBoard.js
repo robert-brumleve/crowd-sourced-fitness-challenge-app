efe875c686201fafc1c50e4b273612024197b7cd
Anh Ho <hodacd@oregonstate.edu>
2024-11-20 23:55:35 -0600

    Added Dashboard description

--- frontend/src/dashboard/DashBoard.js
+++ frontend/src/dashboard/DashBoard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useMemo } from "react";
 import axios from "axios";
 import UserChallengeList from "../components/UserChallengeList";
 import Header from "../components/Header";
+import Greeting from "../components/Greeting";
 
 
 const DashBoard = () => {
@@ -33,6 +34,25 @@ const DashBoard = () => {
 
   return (
     <div>
+       <Greeting />
+      <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">Challenge Dashboard</h1>
+            <p class="lead text-muted">
+              This dashboard displays all the challenges you have
+              participated in. Keep moving!
+            </p>
+            <p>
+              {/* <a href="/placeholder" class="btn btn-primary my-2">My Challenges</a> */}
+              <a href="/challenges" class="btn btn-secondary my-2">
+                Community Challenges
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+
       <Header header="MY CHALLENGES"/>
       <div className="table-responsive">
         <table className="table table-sm table-bordered table-hover">
