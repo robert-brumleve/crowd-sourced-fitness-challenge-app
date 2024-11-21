6fc6f3413a284029deddc86e03f2eda3982bd3f5
Yohan Moon <yohan.moon@outlook.com>
2024-11-21 09:47:48 -0800

    comment out consolelog

--- frontend/src/components/Greeting.js
+++ frontend/src/components/Greeting.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Greeting = () => {
   const username = localStorage.getItem("username");
-  console.log("localStorage", localStorage);
+  //console.log("localStorage", localStorage);
 
   return <h4>Hello, {username ? username : "Guest"}!</h4>;
 };
