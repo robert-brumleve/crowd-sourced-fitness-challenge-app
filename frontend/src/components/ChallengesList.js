d5229608345bf2bcd90f032f57e8397d00f8162f
Yohan Moon <yohan.moon@outlook.com>
2024-11-20 22:31:37 -0800

    removed PlusIcon from import for eslint error

--- frontend/src/components/ChallengesList.js
+++ frontend/src/components/ChallengesList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ViewIcon, PlusIcon } from "./Icons";
+import { ViewIcon } from "./Icons";
 
 const ChallengeList = (props) => {
   return (
